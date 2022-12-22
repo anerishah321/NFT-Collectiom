7a771ba272b0f4b6901d6a7a7c206c094c40077a
anerishah321 <shahaneri167@gmail.com>
2022-12-22 21:18:10 +0530

    first commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+const WHITELIST_CONTRACT_ADDRESS = "0x7DC317A7C9ffD946ad18729919f38e721348C11b";
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
